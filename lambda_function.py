5652252712e5e7af65211b82692e140158abcca3
Dinesh Agarwal <dineshljagarwal@gmail.com>
2023-03-14 13:31:49 +0530

    modify lambda code 4

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello CI/CD Pipeline')
+        'body': json.dumps('CI/CD Pipeline from Code Build!!')
     }
